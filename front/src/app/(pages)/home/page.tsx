c01244221e91df6d8d50d23f0501ea3b47c7dac2
emily <martorana.emily38@gmail.com>
2024-10-01 17:55:49 +0200

    static navbar

---
+++ front/src/app/(pages)/home/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { Navbar } from '@/components/Navbar'
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className='grid grid-cols-3'>
+       <Navbar/> 
+    </div>
+  )
+}
+
+export default home
---
+++ front/src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
+import { FaArrowLeftLong } from 'react-icons/fa6'
+
+export const Navbar = () => {
+    const router = useRouter() 
+    const[isActive, setIsActive]=useState(false)
+    async function search(){
+            setIsActive(!isActive)
+       
+    }
+  return (<div className="">
+    
+    <aside className='bg-white text-black h-lvh w-1/5 fixed  '>
+        <ul className='flex flex-col justify-around items-center h-lvh  '>
+            <Image width={100} height={100} src={'/logo_interest.png'} alt={'logo'} />
+            <li onClick={()=>{
+                router.push('#')
+            }}>Crée une publication</li>
+
+            <li onClick={()=>{
+                router.push('#')
+            }}>Notification</li>
+            <li onClick={()=>{
+                search()
+            }}>
+                Recherche</li>
+            <li>Profile</li>
+            <li>Déconnexion</li>
+        </ul>
+    </aside>
+    <aside className={`${isActive ? 'block translate-x-0' : 'translate-x-[-100%]'} z-10 flex flex-col items-center bg-white text-black h-lvh w-1/5 fixed transform duration-300`}>
+    <p className='flex items-center gap-2 p-3 self-start' onClick={()=>{
+        search()
+    }}><FaArrowLeftLong/>Retour</p>
+    <input placeholder='recherche' className='rounded-md border border-black p-1 text-center outline-none'></input>
+
+    </aside>
+  </div>)
+}
