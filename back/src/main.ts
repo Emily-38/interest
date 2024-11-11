cfbcb103472e2eec9202c7203010df6f4e3cd6d4
Emily-38 <martorana.emily38@gmail.com>
2024-11-11 14:07:42 +0100

    deploy heroku

--- back/src/main.ts
+++ back/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     whitelist: true, 
     forbidNonWhitelisted:true,
   }))
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
